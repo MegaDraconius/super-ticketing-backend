b0babbb25c43e167e83e6c027f2078a9fc5adbed
roouugeer <rogeresteve5@hotmail.com>
2024-07-18 11:20:06 +0200

    feat: Create initial configuration to get ready to work with connection test

--- super-ticketing-backend/Program.cs
+++ super-ticketing-backend/Program.cs
@@ -1,12 +1,19 @@
+using super_ticketing_backend.DataBaseSettings;
+using super_ticketing_backend.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(
+        options => options.JsonSerializerOptions.PropertyNamingPolicy = null);;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.Configure<DataBaseSettings>(
+    builder.Configuration.GetSection("super-ticketing-backend"));
+builder.Services.AddMongoDb(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
