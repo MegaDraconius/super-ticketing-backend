cf1a3c72c52e8f76a2cc8f3ebd65703c56db06bf
SaraJoLo <162146662+SaraJoLo@users.noreply.github.com>
2024-07-25 11:09:49 +0200

    commit to avoid idea folder

--- super-ticketing-backend/Controllers/UserController.cs
+++ super-ticketing-backend/Controllers/UserController.cs
@@ -15,23 +15,32 @@ public class UserController : ControllerBase
 {
     
     private readonly IUserRepository _userRepository;
+    private readonly ICountryRepository _countryRepository;
     private readonly IMapper _mapper;
 
-    public UserController(IUserRepository usersRepository, IMapper mapper)
+    public UserController(IUserRepository usersRepository, IMapper mapper, ICountryRepository countryRepository)
     {
         _userRepository = usersRepository;
         _mapper = mapper;
+        _countryRepository = countryRepository;
     }
-        
+
 
     [HttpGet]
     public async Task<List<UserDto>> Get()
     {
-        var user = await _userRepository.GetAsync();
+        var users = await _userRepository.GetAsync();
+        var userDtos = _mapper.Map<List<UserDto>>(users);
 
-        return _mapper.Map<List<UserDto>>(user);
+        foreach (var userDto in userDtos)
+        {
+            var country = await _countryRepository.GetAsync(userDto.CountryId);
+            userDto.CountryCode = country?.CountryCode;
+        }
+
+        return userDtos;
     }
-       
+
 
     [HttpGet("{id:length(24)}")]
     public async Task<ActionResult<UserDto>> Get(string id)
@@ -44,16 +53,29 @@ public class UserController : ControllerBase
         }
 
         var userDto = _mapper.Map<UserDto>(user);
+        var country = await _countryRepository.GetAsync(userDto.CountryId);
+        userDto.CountryCode = country?.CountryCode;
+        
         return userDto;
     }
 
     [HttpPost]
     public async Task<IActionResult> Post(UserCreateDto userCreateDto)
     {
+        var country = await _countryRepository.GetByNameAsync(userCreateDto.CountryCode);
+        if (country == null)
+        {
+            return BadRequest("Country not found");
+        }
+
         var newUser = _mapper.Map<Users>(userCreateDto);
+        newUser.CountryId = country.Id;
+
         await _userRepository.CreateAsync(newUser);
 
-        var userDto = _mapper.Map<Users>(newUser);
+        var userDto = _mapper.Map<UserDto>(newUser);
+        userDto.CountryCode = country.CountryCode;
+        
         return CreatedAtAction(nameof(Get), new { id = userDto.Id }, userDto);
     }
 
@@ -67,8 +89,21 @@ public class UserController : ControllerBase
             return NotFound();
         }
 
+        if (user.CountryId != updatedUserDto.CountryCode)
+        {
+            var country = await _countryRepository.GetByNameAsync(updatedUserDto.CountryCode);
+            if (country == null)
+            {
+                return BadRequest("Country not found");
+            }
+
+            user.CountryId = country.Id;
+        }
+
         _mapper.Map(updatedUserDto, user);
+
         await _userRepository.UpdateAsync(user);
+
         return NoContent();
     }
 
--- super-ticketing-backend/Utilities/AutoMapperProfile.cs
+++ super-ticketing-backend/Utilities/AutoMapperProfile.cs
@@ -9,17 +9,26 @@ public class AutoMapperProfile : Profile
 {
     public AutoMapperProfile()
     {
-        CreateMap<Tickets, TicketDto>();
+        CreateMap<Tickets, TicketDto>()
+            .ForMember(dest => dest.UserEmail, opt => opt.Ignore())
+            .ForMember(dest => dest.ItGuyEmail, opt => opt.Ignore());
+
         CreateMap<TicketCreateDto, Tickets>();
+        CreateMap<TicketUpdateDto, Tickets>();
+        
 
         CreateMap<Country, CountryDto>();
         CreateMap<CountryCreateDto, Country>();
 
-        CreateMap<ITGuys, ItGuyDto>();
-        CreateMap<ItGuyCreateDto, ITGuys>();
+        CreateMap<ITGuys, ItGuyDto>()
+            .ForMember(dest => dest.CountryCode, opt => opt.Ignore());
+        CreateMap<ItGuyCreateDto, ITGuys>()
+            .ForMember(dest => dest.CountryId, opt => opt.Ignore());
 
-        CreateMap<Users, UserDto>();
-        CreateMap<UserCreateDto, Users>();
+        CreateMap<Users, UserDto>()
+            .ForMember(dest => dest.CountryCode, opt => opt.Ignore());
+        CreateMap<UserCreateDto, Users>()
+            .ForMember(dest => dest.CountryId, opt => opt.Ignore());
     }
     
 }
